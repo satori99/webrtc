d8e67d40404f5cbcc4db2c68385fbb11704d7659
Satori <satori.developer@gmail.com>
2016-10-07 20:10:50 +1100

    disable ws server

--- src/worker.js
+++ src/worker.js
@@ -6,16 +6,16 @@
 
 const http = require( 'http' )
 
-const ws = require( 'uws' )
+// const ws = require( 'uws' )
 
 const app = require( './app' )
 
 const httpServer = http.createServer( app )
 
-const wsServer = new ws.Server( {
-	server: httpServer,
-	port: 
-} )
+// const wsServer = new ws.Server( {
+// 	server: httpServer,
+// 	port: 
+// } )
 
 httpServer.once( 'error', err => {
 
